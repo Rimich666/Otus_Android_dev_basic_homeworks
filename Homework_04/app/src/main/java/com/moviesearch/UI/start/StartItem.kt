a63f4f547abb59a47dac35e455c4f9d780b907af
OEM Configuration (temporary user) <89179699365@mail.ru>
2022-06-29 12:15:16 +0400

    20220629 1215

--- Homework_04/app/src/main/java/com/moviesearch/UI/start/StartItem.kt
+++ Homework_04/app/src/main/java/com/moviesearch/UI/start/StartItem.kt
@@ -1,8 +1,10 @@
 package com.moviesearch.UI.start
 
-class StartItem(
-    var action: String,
-    var result: String
-) {
+class RequestedItem(var action: String, result: String)
 
+class InitCashItem(var aText: String)
+
+sealed class StartItem{
+    class Requested(val requested: Requested) : StartItem()
+    class InitCash(val initCash: InitCashItem): StartItem()
 }
